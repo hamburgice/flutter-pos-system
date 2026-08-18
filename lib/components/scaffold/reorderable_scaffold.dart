170c3d09e652d0990bdd6ebe18533ff826223134
hamburgice <yaojunjay@msn.com>
2026-08-18 23:47:39 +0200

    fix: retain Flutter 3.41 reorder API

--- lib/components/scaffold/reorderable_scaffold.dart
+++ lib/components/scaffold/reorderable_scaffold.dart
@@ -25,7 +25,7 @@ class _ReorderableScaffoldState<T extends ModelOrderable> extends State<Reordera
   Widget build(BuildContext context) {
     Widget child = ReorderableList(
       itemCount: widget.items.length,
-      onReorderItem: _handleReorder,
+      onReorder: _handleReorder,
       onReorderStart: (int index) => HapticFeedback.lightImpact(),
       onReorderEnd: (int index) => HapticFeedback.lightImpact(),
       itemBuilder: (BuildContext context, int index) {
@@ -83,6 +83,9 @@ class _ReorderableScaffoldState<T extends ModelOrderable> extends State<Reordera
 
   bool _handleReorder(int oldIndex, int newIndex) {
     setState(() {
+      if (oldIndex < newIndex) {
+        newIndex -= 1;
+      }
       final draggedItem = widget.items.removeAt(oldIndex);
       widget.items.insert(newIndex, draggedItem);
     });
